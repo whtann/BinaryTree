af5fb0c62908c74d1750595fd4d090b11323bc22
whtann <harrisontann@gmail.com>
2019-08-05 12:48:01 -0500

    Updated comments

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -69,9 +69,11 @@ binaryTree::~binaryTree()
 }
 
 //public member function that allows deletion of the entire tree
+//calls the private member recursive function with access to root
 void binaryTree::deleteTree()
 {
     deleteTree(root);
+    root = nullptr;
 }
 
 //recursive function for deleting tree
@@ -86,28 +88,34 @@ void binaryTree::deleteTree(btnode *leaf)
 }
 
 //public member function that allows for insertion of data into the tree
+//calls private member recursive function with access to the root node
 void binaryTree::insert(int key)
 {
     insert(key, root);
 }
 
-//recursive function that inserts nodes in the tree
+//recursive private member function that inserts nodes in the tree
 void binaryTree::insert(int key, btnode *leaf)
 {
+    //creation of root node
     if (!root)
     {
         root = new btnode;
         root->data = key;
         root->l = root->r = nullptr;
     }
+    //creation of every other node
     else
     {
+        //check for left sided data
         if (key < leaf->data)
         {
+            //check for left node. If it exists run the function recursively
             if (leaf->l)
             {
                 insert(key, leaf->l);
             }
+            //if the left node doesn't exist create a new one
             else
             {
                 leaf->l = new btnode;
@@ -115,12 +123,15 @@ void binaryTree::insert(int key, btnode *leaf)
                 leaf->l->l = leaf->l->r = nullptr;
             }
         }
+        //check for right sided data
         else
         {
+            //check for right node. If it exists run the function recursively
             if (leaf->r)
             {
                 insert(key, leaf->r);
             }
+            //if the right node doesn't exist create a new one
             else
             {
                 leaf->r = new btnode;
@@ -131,11 +142,15 @@ void binaryTree::insert(int key, btnode *leaf)
     }
 }
 
+//public member function that allows for the tree to be printed
+//calls the private member function with access to the root node
 void binaryTree::printTree()
 {
     printTree(root);
 }
 
+//private member function that prints the tree with depth.
+//uses a queue to recognize nodes and print them
 void binaryTree::printTree(btnode *leaf)
 {
     if (!root)
@@ -144,46 +159,56 @@ void binaryTree::printTree(btnode *leaf)
     }
     queue<btnode *> q;
     q.push(root);
+    //run until all nodes have been printed
     while (!q.empty())
     {
         int nodeCount = q.size();
+        //run until all nodes are popped out of the queue
         while (nodeCount > 0)
         {
             btnode *leaf = q.front();
             cout << leaf->data << " ";
             q.pop();
+            //add to the queue if a left node exists
             if (leaf->l)
             {
                 q.push(leaf->l);
             }
+            //add to the queue if a right node exists
             if (leaf->r)
             {
                 q.push(leaf->r);
             }
+            //remove count on the node popped
             nodeCount--;
         }
         cout << endl;
     }
 }
 
-//basic depth first search algorithm
+//basic (recursive) search algorithm
 btnode *binaryTree::search(int key, btnode *leaf)
 {
+    //if the node exists
     if (leaf)
     {
+        //if the key is equal to the data return the node
         if (key == leaf->data)
         {
             return leaf;
         }
+        //if the key is less than the data check the left node
         if (key < leaf->data)
         {
             return search(key, leaf->l);
         }
+        //if the key is more or equal to the data check the right node
         else
         {
             return search(key, leaf->r);
         }
     }
+    //if the node doesn't exist return nullptr.
     else
     {
         return nullptr;
